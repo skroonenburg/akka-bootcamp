ce5e1037ee976d16ba552a32c8093a9eb2e76b42
Sam Kroonenburg <sam.kroonenburg@kiandra.com.au>
2015-02-23 22:35:54 +1100

    Completed Unit 1

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -1,39 +1,54 @@
+// ConsoleReaderActor.cs
+// removing validation logic and changing store actor references
 using System;
 using Akka.Actor;
 
 namespace WinTail
 {
     /// <summary>
-    /// Actor responsible for reading FROM the console. 
+    /// Actor responsible for reading FROM the console.
     /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
     /// </summary>
     class ConsoleReaderActor : UntypedActor
     {
+        public const string StartCommand = "start";
         public const string ExitCommand = "exit";
-        private ActorRef _consoleWriterActor;
-
-        public ConsoleReaderActor(ActorRef consoleWriterActor)
-        {
-            _consoleWriterActor = consoleWriterActor;
-        }
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && read.ToLowerInvariant().Equals(ExitCommand))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
+                DoPrintInstructions();
+            }
+
+            GetAndValidateInput();
+        }
+
+
+        #region Internal methods
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Please provide the URI of a log file on disk.\n");
+        }
+
+
+        /// <summary>
+        /// Reads input from console, validates it, then signals appropriate response
+        /// (continue processing, error, success, etc.).
+        /// </summary>
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (!string.IsNullOrEmpty(message) && message.ToLowerInvariant().Equals(ExitCommand))
+            {
+                // if user typed ExitCommand, shut down the entire actor system (allows the process to exit)
                 Context.System.Shutdown();
                 return;
             }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
+            // otherwise, just hand message off to validation actor (by telling its actor ref)
+            Context.ActorSelection("/user/validationActor").Tell(message);
         }
-
+        #endregion
     }
 }
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Akka.Actor;
 
 namespace WinTail
 {
@@ -7,41 +8,32 @@ namespace WinTail
     {
         public static ActorSystem MyActorSystem;
 
+        // Program.cs
         static void Main(string[] args)
         {
             // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            PrintInstructions();
+            // nothing here where our actors used to be!
+            Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            ActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
 
-            // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
-            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+            var tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            ActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
 
+            var fileValidatorActorProps = Props.Create(() => new FileValidatorActor(consoleWriterActor));
+            ActorRef validationActor = MyActorSystem.ActorOf(fileValidatorActorProps, "validationActor");
+
+            Props consoleReaderProps = Props.Create<ConsoleReaderActor>();
+            ActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
 
             // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.AwaitTermination();
         }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
     #endregion
 }
